db185b58e8f808e61787d9255ce52fae98123cc5
Alexej Tessaro <alexej@depop.com>
2019-04-13 20:01:13 +0200

    Remove unneeded firewall rules when using wired

--- bin/docker/raspbian/firewall.sh
+++ bin/docker/raspbian/firewall.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 iptables -P FORWARD ACCEPT
-iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
-iptables -A FORWARD -i wlan0 -o eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
